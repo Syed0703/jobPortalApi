c53bf95ca63cee765ef4a2a603024c3ac5721b38
Siddiq <syedsiddiq0703@gmail.com>
2026-07-29 00:22:02 +0530

    Added Multer

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { login, logout, regster, updateProfile } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
-router.route("/register").post(regster);
+router.route("/register").post(singleUpload ,regster);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated, updateProfile);
