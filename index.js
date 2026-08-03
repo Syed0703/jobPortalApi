71953b6f6c0edc961bf10b8a3eca3b8b54bc0a8c
Siddiq <syedsiddiq0703@gmail.com>
2026-08-03 22:16:30 +0530

    cors origin setup

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 const corsOption = {
-    origin: 'http://localhost:5173',
+    origin: 'https://careerhub-lac.vercel.app',
     credentials: true,
 }
 app.use(cors(corsOption))
